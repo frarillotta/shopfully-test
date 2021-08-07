b602cafa79038166d6be90989b4e949f48a9e191
frarillotta <frarillotta@gmail.com>
2021-08-08 00:00:09 +0200

    removed unused util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,4 @@
 export { decorateFlyers } from './decorateFlyers';
-export { capitalizeFirstLetter } from './capitalizeFirstLetter';
 export { setItem, getItem } from './localStorage';
 export { debounce } from './debounce';
 export {toMap} from './toMap';
